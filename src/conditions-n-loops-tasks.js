f018d0f4cc613655dc2d8f301760a57079def4cc
sd <sd_net@mail.ru>
2023-12-28 22:46:27 +0300

    feat: add fn convertToRomanNumerals()

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -108,8 +108,28 @@ function isIsoscelesTriangle(a, b, c) {
  *  10  => X
  *  26  => XXVI
  */
-function convertToRomanNumerals(/* num */) {
-  throw new Error('Not implemented');
+function convertToRomanNumerals(num) {
+  const dictionary = new Map([
+    [1, 'I'],
+    [2, 'II'],
+    [3, 'III'],
+    [4, 'IV'],
+    [5, 'V'],
+    [6, 'VI'],
+    [7, 'VII'],
+    [8, 'VIII'],
+    [9, 'IX'],
+    [10, 'X'],
+  ]);
+
+  if (num === 10) return dictionary.get(10);
+  let out = '';
+  const units = num % 10;
+  const dozens = num - units;
+  for (let i = 0; i < dozens; i += 10) {
+    out += dictionary.get(10);
+  }
+  return out + dictionary.get(units);
 }
 
 /**
